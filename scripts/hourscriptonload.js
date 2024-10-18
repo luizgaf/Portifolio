cc408d1438dde4691320fac07c9d2bd40fd929e8
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-10-17 23:56:38 -0300

    whatches

---
+++ scripts/hourscriptonload.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    loop(9, 'hjpn');
+    loop(-3, 'hbsb');
+    loop(-4, 'hnyc');
+    loop(0, 'hgmt');
+    loop(8, 'hbei');
+    loop(11, 'hsyd');
+
+    loopClock(9, '#acjpn');   // Tokyo, Japão
+    loopClock(-3, '#acbsb');  // Brasília, Brasil
+    loopClock(-4, '#acnyc');  // Nova York, EUA
+    loopClock(0, '#aclon');   // Londres, Inglaterra
+    loopClock(8, '#acbei');   // Pequim, China
+    loopClock(11, '#acsyd');  // Sydney, Austrália
+}
