107015f4a707fdb84733bb2f0ce0333a9b0c48db
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-10-23 16:03:13 -0300

    some modifications

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,52 +1,96 @@
 
-    var mainInput = document.getElementById('calc-screen-input');
-    var prevInput = document.getElementById('previous-screen');
-    var firstValue, operationtype, opconcluded, canDot;
+var mainInput = document.getElementById('calc-screen-input');
+var prevInput = document.getElementById('previous-screen');
+var firstValue, secondValue, operationType, opConcluded;
 
 
     function actionButton(buttonValue) {
-        if(opconcluded == 1)clearInput();
+        if(opConcluded == 1)clearInput();
         mainInput.value += buttonValue; 
-        opconcluded = 0;
+        opConcluded = 0;
     }
 
     function operation(type){
-        if(opconcluded = 1)opconcluded = 0;
-        operationtype = type;
+        if(opConcluded = 1)opConcluded = 0;
+        operationType = type;
+    
         firstValue = parseFloat(mainInput.value);
-        if(mainInput.value == ''){firstValue = 0; mainInput.value = 0;}
+    
+        if(mainInput.value == ''){
+            firstValue = 0;
+            mainInput.value = 0;
+        }
+    
         prevInput.value = mainInput.value;
         mainInput.value = '';
-        if(operationtype == 1){prevInput.value += ' + '; }
-        if(operationtype == 4){prevInput.value += ' ÷ '; }
-        if(operationtype == 3){prevInput.value += ' × '; }
-        if(operationtype == 2){prevInput.value += ' - ';} 
+    
+        switch(operationType){
+            case 1:prevInput.value += ' + ';
+                break;
+            case 4:prevInput.value += ' ÷ ';
+                break;
+            case 3:prevInput.value += ' × ';
+                break;
+            case 2:{prevInput.value += ' - ';
+                break;
+            }
+        }
     }
 
 
     function equalButton(){
-        value = parseFloat(mainInput.value);
-        if(operationtype == 1){prevInput.value += value; final = firstValue + value;}
-        if(operationtype == 2){prevInput.value += value; final = firstValue - value;}
-        if(operationtype == 3){prevInput.value += value; final = firstValue * value;}
-        if(operationtype == 4){prevInput.value += value; final = firstValue / value;}
-        opconcluded = 1;
+        if(mainInput.value == '')secondValue = 0;
+        else secondValue = parseFloat(mainInput.value);
+
+        switch(operationType){
+            case 1:{
+                prevInput.value += secondValue; 
+                final = firstValue + secondValue;}
+                break;
+            case 2:{
+                prevInput.value += secondValue; 
+                final = firstValue - secondValue;}
+                break;
+            case 3:{
+                prevInput.value += secondValue; 
+                final = firstValue * secondValue;}
+                break;
+            case 4:{
+                prevInput.value += secondValue; 
+                final = firstValue / secondValue;}
+                break;
+        }
+        opConcluded = 1;
 
         if(Number.isInteger(final))mainInput.value = final;
-        else  mainInput.value = parseFloat(final.toFixed(4)).toString();
+
+        else
+          mainInput.value = parseFloat(final.toFixed(4)).toString();
     }
 
     function clearInput(){
         mainInput.value = '';
         prevInput.value = '';
         firstValue = 0;
-        operationtype = 0;
+        operationType = 0;
     }
 
     function dotInput(){
         if(!mainInput.value.includes('.')){
             mainInput.value += '.';
         }
+    }
+
+    function factorial(value){
+        var factorial;
+        for(var i = value; i > 0; i--){
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    function percent(value){
+        return value/100;
     }
 
     
